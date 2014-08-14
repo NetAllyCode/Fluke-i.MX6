46d9759851b26614060e3ec5bedc12977b6b3734
Sam Dickson <scdickso@purdue.edu>
2014-08-14 17:38:49 -0400

    Update auto_ethernet_gadget_win.sh

--- Configuration/Scripts/auto_ethernet_gadget_win.sh
+++ Configuration/Scripts/auto_ethernet_gadget_win.sh
@@ -2,4 +2,3 @@
 insmod /lib/modules/3.0.35-g1c8e659-dirty/kernel/drivers/usb/gadget/arcotg_udc.ko
 insmod /lib/modules/3.0.35-g1c8e659-dirty/kernel/drivers/usb/gadget/g_ether.ko idVendor=0x0f7e idProduct=0x0006 iManufacturer=\"Fluke Networks\" iProduct=\"USBLAN\"
 ifconfig usb0 169.254.222.5 up
-/root/tcp_server 27015
